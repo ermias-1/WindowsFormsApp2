3b965efd6f6063decd39e7005e020c313b6ff237
Ermias Gebregiorgis <vv8702in@go.minneapolis.edu>
2020-01-23 10:08:09 -0600

    Add project files.

---
+++ WindowsFormsApp2/Form1(BusExpense).cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void texTrips_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double busFare = 2.0;
+            int trips;
+            if (Int32.TryParse(txtTrips.Text, out trips))
+            {
+                double total = busFare * trips;
+                txtTotal.Text = "$" + total;
+            }
+            else
+            {
+                MessageBox.Show("Enter only numbers", "Error");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
